0433722603126c324752022d0ccc37560ae7f5d3
Parminderk766 <51168168+Parminderk766@users.noreply.github.com>
2020-06-16 10:45:19 -0400

    added the scores variable

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-let score = 0;
+var score = 0;
+
+var holes = Array.from(document.getElementsByClassName('hole'))
+
+// var randomHoleIndes = Math.floor(Math.random() * holes.length);
+
+setInterval(function() {holes[Math.floor(Math.random() * holes.length)].classList.toggle('mole')}, 300)
+
+const gameArea = document.getElementById('whack-a-mole');
+
+gameArea.addEventListener('click', function(clickEvent) {
+    if (clickEvent.target.matches('.mole')){
+         score++;
+        document.getElementById('score').innerText = score;
+    } else{
+        score = 0;
+        document.getElementById('score').innerText = score;
+    }
+})
+
